6a0ee40d634c6dcbb9b63dbd1fee3376e90bd481
Isaac Park <isaac.a.park+git@gmail.com>
2020-04-26 19:59:10 -0700

    add ice

--- client/index.ts
+++ client/index.ts
@@ -4,21 +4,17 @@ import Signaler, {Role} from './signaler'
 const s = new Signaler()
 const video: HTMLVideoElement = document.getElementById('video') as HTMLVideoElement;
 
-function messagePromise(websocket: WebSocket): Promise<string> {
-    return new Promise( resolve => {
-        websocket.onmessage = (e) => {
-            resolve(e.data)
-        } 
-    })
-}
-
 async function main() {
     const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
 
     const role = await s.getRole()
     console.log('got role', role)
 
-    var peer = new Peer({initiator: role == Role.initiator, stream: stream})
+    var peer = new Peer({
+        initiator: role == Role.initiator, 
+        stream: stream,
+        config: { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }, { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }] },
+    })
     peer.on('close', () => s.close())
 
     peer.on('signal', data => {
